610a58deb4cf5aa5958d065be561ec02ffac1d85
Guna <24je0601@iitism.ac.in>
2024-10-24 11:57:27 +0530

    Inverse implemented

--- calculator.c
+++ calculator.c
@@ -64,12 +64,12 @@ int main() {
             case '^':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("result is %d\n",a*a);
                 break;
             case '#':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("result is %d\n",a*a*a);
                 break;
             case 'i':
                 printf("Enter an integer: ");
